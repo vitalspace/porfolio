e29efbfcbdcda24849479074f65df0d19d7f2b3b
vitalspace <myglobalsounds@gmail.com>
2023-01-08 01:44:17 -0600

    updated

--- src/componets/blog.svelte
+++ src/componets/blog.svelte
@@ -1,19 +1,26 @@
 <script lang="ts">
-  import Posts from "../lib/posts.json";
+  import Blog from "../lib/posts.json";
+  //@ts-ignore
+  const posts = Blog[1].Blog;
 </script>
 
-<div class="sm:flex  sm:container sm:mx-auto sm:space-x-10 mt-4">
+<div class="sm:flex sm:container sm:mx-auto sm:space-x-10 mt-4">
   <div class="w-full border-2 border-blue-400 h-28 sm:h-60 sm:w-96">
     ajslkdjkasd
   </div>
 
   <div class="w-full flex flex-wrap">
-    {#each Posts as elem}
+    {#each posts as elem}
       <div
         class="w-full sm:mx-1 rounded-t-md sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4"
       >
-        <a href={`/post?id=`+elem.id}>
-          <img class="w-full rounded-t-md" src={elem.img} alt={elem.subject} title={elem.subject} />
+        <a href={`/post?id=` + elem.id}>
+          <img
+            class="w-full rounded-t-md"
+            src={elem.img}
+            alt={elem.subject}
+            title={elem.subject}
+          />
           <div class="bg-zinc-700">
             <header class="py-2 pl-2">
               <h2 class="text-white">{elem.title}</h2>
@@ -29,11 +36,11 @@
         </a>
       </div>
     {/each}
-
-    <!-- <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-400">a</div> -->
-    <!-- <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">a</div> -->
-    <!-- <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-400">a</div> -->
-    <!-- <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 mb-4 bg-gray-500">a</div> -->
-    <!-- <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 mb-4 bg-gray-400">a</div> -->
   </div>
+</div>
+
+<div
+  class="text-white py-4 container mx-auto border-2 border-blue-400 text-center "
+>
+  <a href="/blog?&page=1">1</a>
 </div>
--- src/componets/post.svelte
+++ src/componets/post.svelte
@@ -1,5 +1,71 @@
-<script lang="ts" client:only>
-  import { get } from "svelte/store";
+<script lang="ts">
+  import Blog from "../lib/posts.json";
+
   const url = new URL(window.location.href);
-  console.log(url.search);
+  const dataBase = url.search.split("=");
+  const rute = dataBase[0].split("?")[1];
+  const id = dataBase[1];
+  //@ts-ignore
+  const posts = Blog[1].Blog;
+  //@ts-ignore
+
+  let post: {
+    title: string;
+    subject: string;
+    img: string;
+    tags: string;
+    smallDescription: string;
+    body1: string;
+    body2: string;
+    body3: string;
+    body4: string;
+  };
+
+  if (rute == "id" && id) {
+    post = posts.find((e: string) => e.id == id);
+  } else {
+  }
 </script>
+
+{#if post}
+  <div class="container mx-auto">
+    <div class="blog-post rounded-lg bg-zinc-700 p-8 shadow-lg">
+      <h1 class="mb-4 text-3xl font-bold leading-tight text-white">
+        {post.title}
+      </h1>
+      <h2 class="mb-4 text-xl font-bold text-gray-300">{post.subject}</h2>
+      <div class="blog-post-image mb-8">
+        <img
+          src={post.img}
+          alt={post.title}
+          class="w-full sm:w-1/2 rounded-lg shadow-md"
+        />
+      </div>
+      <div class="blog-post-tags mb-4">
+        <span class="mr-2 font-bold text-gray-400">Tags:</span>
+        <ul class="flex flex-wrap">
+          {#each post.tags as tag}
+            <a
+              class="mr-2 mb-2 rounded-full bg-gray-200 px-3 py-1 font-semibold"
+              href="/tags?tag={tag}"
+            >
+              <li />
+              {tag}</a
+            >
+          {/each}
+        </ul>
+      </div>
+      <p
+        class="blog-post-small-description mb-8 text-sm font-light text-gray-400"
+      >
+        {post.smallDescription}
+      </p>
+      <div class="blog-post-body text-white">
+        <p class="mb-4 text-lg leading-relaxed">{post.body1}</p>
+        <p class="mb-4 text-lg leading-relaxed">{post.body2}</p>
+        <p class="mb-4 text-lg leading-relaxed">{post.body3}</p>
+        <p class="mb-4 text-lg leading-relaxed">{post.body4}</p>
+      </div>
+    </div>
+  </div>
+{/if}
